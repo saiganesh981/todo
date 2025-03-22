be4cb34d70985330f6a178b305fcaba0c17b9fb8
unknown <ganesh@trietreetech.com>
2025-03-22 18:47:43 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-
-
-// import './App.css';
-// import ExpandableList from './Components/ExpanableList';
-
-
-
-
-
-// import SimpleContainer from './Components/Can';
+import './Components/App.css';
 import React,{useState} from 'react'
-import Todolist from './Components/Todolist';
-const App= ()=>{
-  const[task,setTask]=useState("");
-  const[todos,setTodos]=useState([]);
-  const changeHandler=e=>{
-    setTask(e.target.value)
+
+
+const App = () => {
+  
+  const [city,setCity] = useState("");
+  const [result,setResult] = useState("");
+  const changeHandler = e =>{
+    setCity(e.target.value);
   }
-  const submitHandler=e=>{
+  const submitHandler = e =>{
     e.preventDefault();
-    const newTodos=[...todos,task];
-    setTodos(newTodos);
-    setTask("");
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=> response.json()).then(
+        data => {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult("Temperature at "+city+"\n"+Math.round(celcius)+"°C");
+        }
+      ).catch(error => console.log(error))
+      setCity("");
   }
-  const deleteHandler = (indexValue)=>{
-  const newTodos=todos.filter((todo,index)=>index!==indexValue)
-  setTodos(newTodos);
-  }
-  return(
+
+  return (
     <div>
       <center>
-        <div className='card'>
-          <div className='card-body'>
-            <h5 className='card-title'>Todo Management Application</h5>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Weather App</h4>
             <form onSubmit={submitHandler}>
-              <input type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
-              <input type="submit" value="Add" name='Add'/>
-            </form>
-            <Todolist todolist={todos} deleteHandler={deleteHandler}/>
+              <input size="30" type="text" name="city" onChange={changeHandler} value={city}/> <br /><br />
+              <input type="submit" value="Get Temperature" 
+              style={{ backgroundColor: "blue", color: "White", padding: "10px 20px", border: "none", cursor: "pointer" }} />
+            </form><br /> <br />
+            <div>
+               <h1>{result}</h1> 
+            </div>
           </div>
         </div>
       </center>
@@ -44,6 +43,4 @@ const App= ()=>{
   )
 }
 
-
-
-export default App;
+export default App
